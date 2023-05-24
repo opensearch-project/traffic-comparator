6d5ad1163b1f599353bbd0e0cec20f9606b86031
Mikayla Thompson <thomika@amazon.com>
2023-05-23 21:16:47 -0600

    Add dockerfile and run script
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>

---
+++ docker/run_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install -e .
+while true
+do
+  nc -v -l -p 9220 | tee /dev/stderr | trafficcomparator -vv stream | trafficcomparator stream-report
+  >&2 echo "Command has encountered error. Restarting now ..."
+  sleep 1
+done
+
